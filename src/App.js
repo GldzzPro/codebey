3730446091fb7b0baf4b1208a4b292be883c31ae
Farhat Baaroun <golden.farhat@gmail.com>
2022-11-17 10:22:14 +0100

    first push
    home animation and the login with jwt token and react context and router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React ,{ useEffect , useRef , useState ,useLayoutEffect} from "react";
+import { gsap } from "gsap";
+// import "./App.css" ;
+import MainLayout from "./layouts/Main";
+import Courses from "./layouts/courses";
+import Course from "./layouts/course";
+import Home from "./layouts/home";
+import Login from "./layouts/Login";
+import Register from "./layouts/Register";
+import Admin from './components/Admin';
+import Unauthorized from './components/Unauthorized';
+import RequireAuth from './components/RequireAuth';
+import { Application } from 'react-rainbow-components';
+import { Route, Routes} from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+
+export default function App() {
+  return <>
+  
+  <Routes>
+  <Route path="/" element={<MainLayout />}>
+   <Route path="login" element={<Login />}/>
+   <Route path="register" element={<Register />}/>
+   <Route path="unauthorized" element={<Unauthorized />} />
+   <Route index element={<Home />} />
+   <Route element={<RequireAuth allowedRoles={['User','Admin']} />}>
+          <Route path="courses" element={<Courses />} />
+   </Route>
+   <Route element={<RequireAuth allowedRoles={['Admin']} />}>
+          <Route path="courses/python" element={<Course/>} />
+        </Route>
+   <Route element={<RequireAuth allowedRoles={['Admin' ]} />}>
+          <Route path="admin" element={<Admin />} />
+        </Route>
+  </Route>
+  </Routes>
+  </>
+    
+  
+};
+        {/* If the current URL is /about, this route is rendered
+            while the rest are ignored */}
+       
+      
+      
+
+
+
+
+          
---
+++ src/api/axios.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+const BASE_URL = 'http://192.168.1.13:8080/api';
+
+export default axios.create({
+    baseURL: BASE_URL
+});
+
+export const axiosPrivate = axios.create({
+    baseURL: BASE_URL,
+    headers: { 'Content-Type': 'application/json' },
+    withCredentials: true
+});
